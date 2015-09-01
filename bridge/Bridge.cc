d5266eb15f2c6e58d8e128fb185ceeb141def7bc
Georgios <georgios@georgios-VirtualBox.(none)>
2015-09-01 18:00:06 +0100

    CHANGES ONCE AGAIN

--- bridge/Bridge.cc
+++ bridge/Bridge.cc
@@ -121,14 +121,8 @@ void Bridge::send(Packet_t packet, int tag) {
 	/* A handle to a request object used for quering the status of the send operation */
 	MPI_Request req;
 
-	/* Lock */ // TODO: Remove
-	//pthread_spin_lock(&(bridge->recv_lock));
-
 	/* Send the MPI message */
 	MPI_Isend(&packet.front(), packet.size(), MPI_UINT64_T, to_rank, tag, *comm_ptr, &req);
-
-	/* Lock */ //TODO: Remove
-	//pthread_spin_unlock(&(bridge->recv_lock));
 
 	/* Flag that indicates the status of the send operation */
 	int flag;
@@ -157,15 +151,12 @@ void Bridge::send(Packet_t packet, int tag) {
 		/* Send the float array */
 		MPI_Isend(arr, data_size, MPI_FLOAT, to_rank, tag, *comm_ptr, &req);
 
-		
-
 		/* Wait until the whole message is sent (this does not necessarily mean it was also received) */
 		do { 
 			MPI_Test(&req, &flag, MPI_STATUS_IGNORE);
-
 		} while (!flag);
 		/* Delete the dynamically allocated float array, it is no longer needed on this process */
-		delete arr;
+		delete [] arr;
 	}
 
 #ifdef VERBOSE
@@ -181,7 +172,7 @@ void Bridge::send_th(Packet_t packet, int tag) {
 	pthread_t thread;
 	pthread_attr_t attr;
 	pthread_attr_init(&attr);
-	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE); // TODO: Perhaps detached?
+	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE);
 
 	/* Create a bridge_packet_tag element which will be passed to a thread as an argument */
 	struct bridge_packet_tag parameters_ = {
@@ -199,6 +190,8 @@ void Bridge::send_th(Packet_t packet, int tag) {
 		printf("Rank %d: send thread could not be created. Exiting program...\n", rank);
 		exit(1);
 	}
+	
+	/* Destroy the attr element. This does not affect the thread */
 	pthread_attr_destroy(&attr);	
 }
 
@@ -212,44 +205,16 @@ int Bridge::get_rank(){
 	return rank;
 }
 
-// Initiates a stencil operation
-/*void Bridge::stencil(std::vector<Packet_t> packet_list){ // TODO: Remove?
-
-#ifdef VERBOSE
-	stringstream ss;
-	ss << "Rank " << rank << "(stencil): Scattering " << packet_list.size() << " packet(s) among " << neighbours.size() << " neighbour(s)\n";
-	cout << ss.str();
-#endif // VERBOSE
-
-	pthread_t thread;
-	pthread_attr_t attr;
-	pthread_attr_init(&attr);
-	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_JOINABLE); // TODO: remove? detach?
-
-	// Create a bridge_packets_parameters element which will be passed to a thread as an argument 
-	struct bridge_packets parameters_ = {
-		this,
-		packet_list
-	};
-
-	// Since pthreads take only args of type void* store the struct on the free store and pass a pointer to the thread 
-	// The free store is used so that the thread can have access to the struct 
-	struct bridge_packets *parameters = new struct bridge_packets(parameters_);
-	int rc = pthread_create(&thread, &attr, stencil_operation_th, (void *) parameters);
-	if (rc) {
-		printf("Rank %d: stencil operation thread could not be created. Exiting program...\n", rank);
-		exit(1);
-	}
-	pthread_attr_destroy(&attr);	
-}*/
-
-// The function executed by a different thread, used for listening for incoming MPI messages
+// The function to be executed in a different thread, used for listening for incoming MPI messages
 // The thread is created in the constructor of Bridge
 void* wait_recv_any_th(void *arg){
 	Bridge *bridge = (Bridge*) arg;
 	MPI_Status status;
 
+	/* Reference to the System instance */
 	System& sba_system = *((System*)bridge->sba_system_ptr);
+
+	/* Pointer to the communicator */
 	MPI_Comm *comm_ptr = sba_system.get_comm_ptr();
 
 	/* Used for checking the output of MPI_Iprobe() and MPI_Test() */
@@ -280,16 +245,6 @@ void* wait_recv_any_th(void *arg){
 			continue;
 		}
 
-		/* Check the tag of the incoming message. If it's a special-purpose tag (e.g. tag_stencil_reduce) maybe some other 
-		 * should deal with this message */ 
-		int tag = status.MPI_TAG; // TODO: Remove?
-		if (tag == tag_stencil_reduce) {
-		
-			/* Incoming message to be handled by non-receiving thread, unlock and continue*/
-			pthread_spin_unlock(&(bridge->recv_lock));
-			continue; // Let the stencil_operation_th() function do this work
-		}
-
 		Packet_t packet;
 
 		/* Find the packet size */
@@ -300,10 +255,11 @@ void* wait_recv_any_th(void *arg){
 		/* Handle to request object. Used for querying the status of the reciving operation */
 		MPI_Request req;
 
+		/* Receive the probed message */
 		MPI_Irecv(&packet.front(), recv_size, MPI_UINT64_T, status.MPI_SOURCE, status.MPI_TAG,
 					*comm_ptr, &req);
 	
-		/* Receiving has started, synchronisation is no longer required, unlock */
+		/* Receiving has started, unlock */
 		pthread_spin_unlock(&(bridge->recv_lock));
 
 #else // if MPI_VERSION >= 3
@@ -315,7 +271,7 @@ void* wait_recv_any_th(void *arg){
 		MPI_Improbe(MPI_ANY_SOURCE, tag_default, *comm_ptr, &flag, &msg, &status);
 
 		if (!flag){
-			/* No incoming message, continue */
+			/* No incoming message, continue (i.e. listen again for incoming messages) */
 			continue;
 		}
 
@@ -324,12 +280,16 @@ void* wait_recv_any_th(void *arg){
 
 		/* Finds the packet size */
 		int recv_size;
+		/* MPI routine that returns the number of elements of type unsigned int64 in the incoming message */
 		MPI_Get_count(&status, MPI_UINT64_T, &recv_size);
+
+		/* Resize the packet variable so that it can hold exactly the incoming packet */
 		packet.resize(recv_size);
 
-		/* Handle to request object. Used for querying the status of the reciving operation */
+		/* Handle to request object. Used for querying the status of the receiving operation */
 		MPI_Request req;
 
+		/* Receive the probed message */
 		MPI_Imrecv(&packet.front(), recv_size, MPI_UINT64_T, &msg, &req);
 
 
@@ -351,13 +311,16 @@ void* wait_recv_any_th(void *arg){
 		Header_t header = getHeader(packet);
 
 #ifdef EVALUATE
+		/* Perform operations that should ONLY take place when the EVALUATE macro is defined. This macro should never be used during
+		 * actualy deployment. It is only for testing purposes */
 
 		/* Check if the packet received is of type DACK. This is used as an ack packet for evaluation purposes */
 		if (getPacket_type(header) == P_DACK) { 
 
-
+			/* Get time from arbitrary point in the past */
 			double end = MPI_Wtime();
-
+	
+			/* Get the starting time */
 			double start = sba_system.start_time;
 
 	/* These times have meaning only for the test_time_dresp() test and should be ignored otherwise */
@@ -367,9 +330,12 @@ void* wait_recv_any_th(void *arg){
 		#else
 			printf("until invoking the Bridge::send() method: %f \n", sba_system.send_fcn_start - start);
 		#endif // THREADED_SEND
-			printf("until receiving: %f\n", end-start);
+
+			/* Print the total time required for this message to be sent, received and an ack to be sent to the original sender */
+			printf("RANK %d: TIME: %fsecs\n", sba_system.get_rank(), end-start); 
 	#endif // VERBOSE
-			printf("RANK %d: TIME: %fsecs\n", sba_system.get_rank(), end-start); //TODO: uncomment?
+
+			//printf("RANK %d: TIME: %fsecs\n", sba_system.get_rank(), end-start);  //TODO: Remove (it's above)
 
 			sba_system.end_time += end - start;
 
@@ -379,42 +345,73 @@ void* wait_recv_any_th(void *arg){
 		}
 #endif //EVALUATE
 
-		int return_to = (int) getTo(getHeader(packet));
-		int to = (int) getReturn_to(getHeader(packet));
+		/* Get node_id of receiver (stored on the packet header) */
+		int receiver_node_id = (int) getTo(getHeader(packet));
 
+		/* Get node_id of sender (stored on the packet header) */
+		int sender_node_id = (int) getReturn_to(getHeader(packet));
 
+		/* if the GMCF packet received is of type DRESP there must be another MPI message coming that holds a float array.
+		 * Wait for that packet 
+		 */
 		if (getPacket_type(header) == P_DRESP){
 
-			int tag = create_tag(tag_dresp_data, return_to, to);
+			/* Create the tag that the expected MPI message will have. It is a combination of the sender ID, receiver ID and the
+			 * type of the tag (tag_dresp_data) */
+			int tag = create_tag(tag_dresp_data, receiver_node_id, sender_node_id);
 			
+#if MPI_VERSION<3
+
+			/* No locking is required, we do not need to synchronise MPI_Iprobe() and MPI_Irecv() because only the expected 
+			 * message should have the aforementioned tag
+			 */
+
+			/* Test for a message, but don't receive */ 			
+			do{
+				MPI_Iprobe(MPI_ANY_SOURCE, tag, *comm_ptr, &flag, &status);
+			}while (!flag);
+
+			/* Packet detected. Find the size of the float array, receive the message and store the array */
+			MPI_Get_count(&status, MPI_FLOAT, &recv_size);
+			float *arr = new float[recv_size];
+
+			/* Receive the probed message */
+			MPI_Irecv(arr, recv_size, MPI_FLOAT, status.MPI_SOURCE, tag,
+						*comm_ptr, &req);
+			do {
+				MPI_Test(&req, &flag, &status);
+			} while (!flag);
+
+#else // if MPI_VERSION>=3
+
+			/* Wait for the message */
 			flag = false;
 			while (!flag){
 				MPI_Improbe(MPI_ANY_SOURCE, tag, *comm_ptr, &flag, &msg, &status);
-
 			}
+
+			/* Packet detected. Find the size of the float array, receive the message and store the array */
 			MPI_Get_count(&status, MPI_FLOAT, &recv_size);
 			float *arr = new float[recv_size];
 			MPI_Imrecv(arr, recv_size, MPI_FLOAT, &msg, &req);
-
 			do {
 				MPI_Test(&req, &flag, &status);
-
 			} while (!flag);
 
+#endif // MPI_VERSION<3
 
-
+			/* At this point the float array has been received */
 		}
 
 #ifdef EVALUATE	
 		/* If the DRESP packet detected was used for evaluation purposes, don't store it. Instead, send a DACK packet back and keep 
-		 * listening for messages */
+		 * listening for messages. When EVALUATE is defined the packets will NOT go to the RX FIFO of the receiving tile */
 
-		/* Add the start time in Word form to the payload */
 		Payload_t payload;
 		payload.push_back((Word)999);
 
 		/* Create the header of the GMCF packet. This function is part of the original GMCF code */
-		header = mkHeader(P_DACK, 2, 3, payload.size(), to, return_to, 7 , 0);
+		header = mkHeader(P_DACK, 2, 3, payload.size(), sender_node_id, receiver_node_id, 7 , 0);
 
 		/* Create the GMCF packet. This function is part of the original GMCF code */
 		packet = mkPacket(header, payload);
@@ -436,7 +433,6 @@ void* wait_recv_any_th(void *arg){
 #ifdef VERBOSE
 		printf("Rank %d (Recv): Sent packet to dest %d\n", sba_system.get_rank(), dest);
 #endif // VERBOSE
-
 	}
 
 	/* System destructor has been called. Notify the System that this thread will be killed */
@@ -522,13 +518,12 @@ void* send_th_fcn(void *args) {
 		do { 
 			MPI_Test(&req, &flag, MPI_STATUS_IGNORE);
 		} while (!flag);
-
 		/* Delete the dynamically allocated float array, it is no longer needed on this process */
-		delete arr;
+		delete [] arr;
 	}
 
 #ifdef VERBOSE
-	printf("Rank %d (Sent): Sent a packet to %d\n", sba_system.get_rank(), to_rank);
+	printf("Rank %d (Threaded Sent): Sent a packet to %d\n", sba_system.get_rank(), to_rank);
 #endif // VERBOSE
 
 
@@ -537,57 +532,3 @@ void* send_th_fcn(void *args) {
 
 	pthread_exit(NULL);
 }
-
-//A function that will be executed by a thread which will initiate a stencil operation
-/*void* stencil_operation_th(void *args){ //TODO: Remove?
-
-	//Timeit
-	double start_time = MPI_Wtime();
-	struct bridge_packets *parameters;
-	parameters = (struct bridge_packets *) args;
-	std::vector<Packet_t> packet_list = parameters->packet_list;
-	Bridge *bridge = parameters->bridge;
-	printf("Rank %d: starting stencil operation\n", bridge->rank);
-
-	int sum=0;
-	MPI_Status status;
-	int flag;
-
-	System& sba_system = *((System*) bridge->sba_system_ptr);
-	MPI_Comm *comm_ptr = sba_system.get_comm_ptr();
-
-	for (unsigned int i = 0; i < packet_list.size() ; i++){
-		bridge->send(bridge->neighbours.at(i%bridge->neighbours.size()), packet_list.at(i), tag_stencil_scatter);
-#ifdef VERBOSE
-		printf("RANK %d: send a packet to ??? (STENCIL)\n", bridge->rank); // TODO:Fix
-#endif //VERBOSE
-	}
-
-	for (unsigned int i = 0; i < packet_list.size(); i++){
-		do { // waits for a msg but doesn't receive
-			MPI_Iprobe(bridge->neighbours.at(i%bridge->neighbours.size()), tag_stencil_reduce,
-			*comm_ptr, &flag, &status);
-		}
-		while (!flag);
-
-		Packet_t packet;
-		//find size of packet
-		int recv_size;
-		MPI_Get_count(&status, MPI_UINT64_T, &recv_size);
-		packet.resize(recv_size);
-
-		MPI_Request request;
-		MPI_Irecv(&packet.front(), recv_size, MPI_UINT64_T, status.MPI_SOURCE, tag_stencil_reduce,
-					*comm_ptr, &request);
-
-		do { // waits until the whole message is received
-			MPI_Test(&request, &flag, &status);
-		} while (!flag);
-		printf("RANK %d: received tag_stencil_reduce from %d\n", bridge->rank, status.MPI_SOURCE);
-		sum += packet.at(0); //TODO: What to do with the packets received?
-	}
-	printf("RANK %d: sum = %d\n", bridge->rank, sum);
-	printf("Rank %d: STENCIL Thread exiting... (%fs)\n", bridge->rank, MPI_Wtime() - start_time);
-	delete parameters; // Free the space allocated on the heap
-	pthread_exit(NULL);
-} */
